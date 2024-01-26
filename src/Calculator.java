390e8ed5f48d83092cb9f4cd3ad805f1d5d64180
Marta Kowalczyk <martusia@mat.umk.pl>
2024-01-26 18:08:26 +0100

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    public int addAToB(int a, int b) {
+        return a + b;
+    }
+
+    public int subtractAFromB(int a, int b) {
+        return a - b;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        System.out.println(calculator.addAToB(1,3));
+
+        System.out.println(calculator.subtractAFromB(1,3));
+    }
+}

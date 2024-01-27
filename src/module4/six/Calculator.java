6818dcbcbfa3d3b498f0288a7638b08d467424bd
Marta Kowalczyk <martusia@mat.umk.pl>
2024-01-27 23:22:02 +0100

    Updating Calculator.java

--- src/module4/six/Calculator.java
+++ src/module4/six/Calculator.java
@@ -9,7 +9,7 @@ public class Calculator {
         return a - b;
     }
 
-    public int multiplyAByB(int a, int b) {
-        return a * b;
+    public int divideAByB(int a, int b) {
+        return a / b;
     }
 }
